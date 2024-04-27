f711c3fae6cb8e6011e0d94458ca00d381b10603
elizabethe56 <78177942+elizabethe56@users.noreply.github.com>
2024-04-26 23:20:12 -0400

    Update app.py

---
+++ src/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import numpy as np
+
+class App:
+
+    def __init__(self):
+        st.session_state['hi'] = 'Test'
+        return
+    
+    def window(self):
+        input_form = st.form('Input')
+
+        input_text = input_form.text_area(label='Type a poem:', key='input_text')
+        
+        input_submit = input_form.form_submit_button('Submit!')
+        st.session_state['help'] = input_text
+        
+        rand = np.random.random()
+        if rand > 0.5:
+            result = 'Woman'
+        else:
+            result = 'Man'
+        st.text(result)
+        # st.text(st.session_state)
+        return
